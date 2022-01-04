2e133cfcc9001f9adc1849548b49cf6892a4050f
mmagdy <mmagdy@flynas.com>
2022-01-04 12:25:51 +0300

    add default text formatter

---
+++ src/AWS.Logger.SeriLog/DefaultAWSCloudWatchTextFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Serilog.Events;
+using Serilog.Formatting;
+
+namespace AWS.Logger.SeriLog
+{
+  /// <summary>
+  /// Default class for aws cloudwatch log text formatter
+  /// </summary>
+  public class DefaultAWSCloudWatchTextFormatter : ITextFormatter
+  {
+    /// <summary>
+    /// Format the message written to cloudwatch logs
+    /// </summary>
+    /// <param name="logEvent"></param>
+    /// <param name="output"></param>
+    public void Format(LogEvent logEvent, TextWriter output)
+    {
+      output.Write("{0}|{1}| {2} {3}", logEvent.Timestamp, logEvent.Level, logEvent.MessageTemplate, output.NewLine);
+      if (logEvent.Exception != null)
+      {
+        output.Write("Exception - {0}", logEvent.Exception);
+      }
+    }
+  }
+}
